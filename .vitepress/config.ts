49e61336a4ad9c99131abe35ece1db87712d30fa
lucas5g <lucasdesousa19@hotmail.com>
2026-02-28 18:26:47 -0300

    test config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
         ['style', {}, '.vp-doc p, .vp-doc li { text-align: justify; }; ']
     ],
     rewrites: {
-        'README.md': 'index.md'
+        'README.md': 'index.md',
+        'docs/:path*': ':path*'
     },
     vite: {
         server: {
@@ -31,10 +32,10 @@ export default defineConfig({
                 // text: 'Guias',
                 items: [
                     { text: 'Início', link: '/' },
-                    { text: 'Integração IDE e Figma', link: '/docs/CONEXAO' },
-                    { text: 'Testes Automatizados por IA', link: '/docs/TESTES' },
-                    { text: 'IA/CI/CD', link: '/docs/IACICD' },
-                    { text: 'Dados Internos e IA', link: '/docs/RAG' }
+                    { text: 'Integração IDE e Figma', link: '/CONEXAO' },
+                    { text: 'Testes Automatizados por IA', link: '/TESTES' },
+                    { text: 'IA/CI/CD', link: '/IACICD' },
+                    { text: 'Dados Internos e IA', link: '/RAG' }
                 ]
             }
         ]
