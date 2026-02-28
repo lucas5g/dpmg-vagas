e74b0a9e1678fea42cf31c7fdd7c5fa29f2f56d0
lucas5g <lucasdesousa19@hotmail.com>
2026-02-28 18:19:13 -0300

    fix(docs): remove rewrites and update sidebar to fix Vitepress routing on Vercel

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
         ['style', {}, '.vp-doc p, .vp-doc li { text-align: justify; }; ']
     ],
     rewrites: {
-        'README.md': 'index.md',
-        'docs/:path*': ':path*'
+        'README.md': 'index.md'
     },
     vite: {
         server: {
@@ -32,10 +31,10 @@ export default defineConfig({
                 // text: 'Guias',
                 items: [
                     { text: 'Início', link: '/' },
-                    { text: 'Integração IDE e Figma', link: '/CONEXAO' },
-                    { text: 'Testes Automatizados por IA', link: '/TESTES' },
-                    { text: 'IA/CI/CD', link: '/IACICD' },
-                    { text: 'Dados Internos e IA', link: '/RAG' }
+                    { text: 'Integração IDE e Figma', link: '/docs/CONEXAO' },
+                    { text: 'Testes Automatizados por IA', link: '/docs/TESTES' },
+                    { text: 'IA/CI/CD', link: '/docs/IACICD' },
+                    { text: 'Dados Internos e IA', link: '/docs/RAG' }
                 ]
             }
         ]
