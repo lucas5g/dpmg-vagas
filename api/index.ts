ac2384d8f2a911510b6d1932108339fd4fa78fa9
lucas5g <lucasdesousa19@hotmail.com>
2026-03-02 15:17:48 -0300

    test index.ts

--- api/index.ts
+++ api/index.ts
@@ -33,6 +33,9 @@ await sql`
   )
 `;
 
+let test = 'lucas'
+console.log(test)
+
 // Exportação direta sem variável intermediária — evita TDZ no Bun/Vercel
 const app = new Elysia({ prefix: '/api' })
     .use(cors()) // Habilitando CORS para o Frontend em React
